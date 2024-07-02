e87518ab4905eb469190f2ad04c86e515a88e88a
rezaawrn <muhammadrezaaswarin.tkj2020@gmail.com>
2024-07-02 12:00:19 +0800

    add : Im Poliban

--- 2 JULI.cpp
+++ 2 JULI.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  cout << "Hello World!";
-  return 0;
+int main()
+{
+    cout << "Hello World!";
+    cout << "Im Poliban";
+    return 0;
 }
